d0cfe14aea0d81b8aa3cdd8278be914830623e72
Jorge Eduardo Ardila <jea.data@gmail.com>
2023-09-04 18:53:37 -0500

    added server

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-const {express} = require('express');
+const express = require('express');
+const path = require('path');
 
-const app = express.app();
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`... listening on port ${PORT}`);
+});
