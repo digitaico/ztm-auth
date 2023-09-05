bb218da9695f19acb80e734ddceb3a1eba59f6a0
Jorge Eduardo Ardila <jea.data@gmail.com>
2023-09-05 11:06:25 -0500

    ca certificate tls

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const https = require('https');
+const helmet = require('helmet');
+const express = require('express');
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(helmet());
+
+app.get('/secret', (req, res) => {
+  res.send(`... Secret is ${req.query}`);
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
-app.listen(PORT, () => {
+https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
   console.log(`... listening on port ${PORT}`);
 });
